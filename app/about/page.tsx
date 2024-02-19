4e635a7e9b3b4300298fe3ccdbaaa5dd3537c44a
Samuel Palacios <samuelpl0888@gmail.com>
2024-02-18 22:45:11 -0400

    Add navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main className='space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32'>
+      <h1 className='font-semibold text-3xl md:text-7xl'>About</h1>
+    </main>
+  );
+}
