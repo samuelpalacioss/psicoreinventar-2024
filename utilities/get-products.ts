15bd952da72212cd692f2f0348306e63079ab23f
Samuel Palacios <samuelpl0888@gmail.com>
2024-02-23 20:20:18 -0400

    pending: server action

--- utilities/get-products.ts
+++ utilities/get-products.ts
@@ -1,6 +1,10 @@
 export default async function getProducts() {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
+      next: {
+        revalidate: 60, // !Revalidate for now (pending server actions to invalidate cache)
+      },
+    });
     if (!response.ok) {
       throw new Error('Something went wrong');
     }
