3ccc7313fef0ccdb8f575ddfde9e9f94b0cb738f
Samuel Palacios <samuelpl0888@gmail.com>
2024-06-10 16:36:11 -0400

    update: styling stats section

--- components/stats.tsx
+++ components/stats.tsx
@@ -6,12 +6,12 @@ interface StatsProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export default function Stats({ stats }: StatsProps) {
   return (
-    <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-      <div className='mx-auto max-w-2xl lg:max-w-none'>
-        <dl className='grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4'>
+    <div className='mx-auto max-w-7xl'>
+      <div className='mx-auto md:p-5 lg:p-0 lg:max-w-none'>
+        <dl className='grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center md:grid-cols-4'>
           {stats.length > 0 &&
             stats.map((stat, i) => (
-              <div key={i} className='flex flex-col p-8 gap-4'>
+              <div key={i} className='flex flex-col lg:p-8 gap-4'>
                 <dt className='text-md leading-6 text-gray-600'>{stat.description}</dt>
                 <dd className='order-first text-3xl font-semibold tracking-tight text-gray-900'>
                   {stat.value}
