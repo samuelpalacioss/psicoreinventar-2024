a7e257c88e17b838c2634e2be0079afd4b3e4b8b
Samuel Palacios <samuelpl0888@gmail.com>
2024-08-28 00:21:08 -0400

    fix: missing suspense

--- app/(doctor)/doctor-register/page.tsx
+++ app/(doctor)/doctor-register/page.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/lib/db';
 import RegisterDoctorForm from '@/components/register-doctor-form';
 import { Option } from '@/components/ui/multiple-selector';
+import { Suspense } from 'react';
 
 async function getSpecialties() {
   const specialties = await prisma.specialty.findMany();
@@ -14,5 +15,9 @@ export default async function RegisterDoctorPage() {
     label: specialty.name,
     value: specialty.name,
   }));
-  return <RegisterDoctorForm specialties={options} />;
+  return (
+    <Suspense>
+      <RegisterDoctorForm specialties={options} />
+    </Suspense>
+  );
 }
