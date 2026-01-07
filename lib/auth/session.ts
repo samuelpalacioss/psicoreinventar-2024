2dd79cacd596242425dac2254dede707eeea39a8
Samuel <samuelpl0888@gmail.com>
2026-01-06 21:57:47 -0400

    update: keep log to monitoring

--- lib/auth/session.ts
+++ lib/auth/session.ts
@@ -4,6 +4,7 @@ import { headers } from "next/headers";
 
 // For server components getCurrentUser
 export const getServerSession = cache(async () => {
+  console.log("getServerSession called");
   const session = await auth.api.getSession({
     headers: await headers(),
   });
